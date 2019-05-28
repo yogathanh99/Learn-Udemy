33f8eba5b1129164b9a6789989182abf23be48d6
yogathanh99 <yogathanh98@gmail.com>
2019-05-28 09:06:39 +0700

    [ADD] Assignment 1 - user-input-output and Modified hook-app

--- ReactJS/hook-app/src/App.js
+++ ReactJS/hook-app/src/App.js
@@ -10,20 +10,37 @@ const App = props => {
     ]
   });
 
-  const handleState = () => {
+  const handleState = (newName) => {
     personSetState({
       persons: [
-        { name: "Thanh2",age: 20},
+        { name: newName,age: 20},
         { name: "Thanh1", age: 22 }
       ]
     })
   }
 
+  const changeValue = (e) => {
+    personSetState({
+      persons: [
+        { name: e.target.value,age: 20},
+        { name: "Thanh1", age: 23 }
+      ]
+    })
+  }
+
+  const style = {
+    backgroundColor: 'white',
+    font: 'inherit',
+    border: '1px solid blue',
+    padding: '8px',
+    cursor: 'pointer'
+  }
+
   return (
     <div className="App">
-      <button onClick={handleState}>Click me!</button>
-      <Person name={personState.persons[0].name} age={personState.persons[0].age}/>
-      <Person name={personState.persons[1].name} age={personState.persons[1].age}/>
+      <button style={style} onClick={()=>handleState('Thanh2')}>Click me!</button>
+      <Person change={changeValue} name={personState.persons[0].name} age={personState.persons[0].age}/>
+      <Person click={() => handleState('Thanh3')} name={personState.persons[1].name} age={personState.persons[1].age}/>
     </div>
   );
 }
--- ReactJS/hook-app/src/Person/Person.js
+++ ReactJS/hook-app/src/Person/Person.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
+import './Person.css'
+
 const Person = props => {
   return (
-    <div className="person">
-      I'm {props.name} and {props.age} years old
+    <div onClick={props.click} className="person">
+      <p>I'm {props.name} and {props.age} years old</p>
+      <input type='text' onChange={props.change} value={props.name}/>
     </div>
   )
 }
