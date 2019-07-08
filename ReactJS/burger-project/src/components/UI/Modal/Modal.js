7aa32ddb16f76ab45bb3163a5d863f709065e706
yogathanh99 <vtthanh@apcs.vn>
2019-07-09 00:08:34 +0700

    [ADD]  Complete first app

--- ReactJS/burger-project/src/components/UI/Modal/Modal.js
+++ ReactJS/burger-project/src/components/UI/Modal/Modal.js
@@ -4,22 +4,28 @@ import classes from './Modal.css'
 import Aux from '../../../hoc/Aux'
 import Backdrop from '../Backdrop/Backdrop'
 
-const Modal = props => {
-  const { show, modalClosed, children } = props
-  return (
-    <Aux>
-      <Backdrop show={show} clicked={modalClosed} />
-      <div
-        className={classes.Modal}
-        style={{
-          transform: show ? 'translateY(0)' : 'translateY(-100vh)',
-          opacity: show ? '1' : '0'
-        }}
-      >
-        {children}
-      </div>
-    </Aux>
-  )
+class Modal extends React.Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show
+  }
+
+  render() {
+    const { show, modalClosed, children } = this.props
+    return (
+      <Aux>
+        <Backdrop show={show} clicked={modalClosed} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: show ? '1' : '0'
+          }}
+        >
+          {children}
+        </div>
+      </Aux>
+    )
+  }
 }
 
 export default Modal
