f1c0355f601e5a4a0a28168a17bedaa1c36eaf34
yogathanh99 <vtthanh@apcs.vn>
2019-07-10 01:09:28 +0700

    [ADD] Complete app-2 -> Learn Ajax by using Axios

--- ReactJS/app-2/src/components/NewPost/NewPost.js
+++ ReactJS/app-2/src/components/NewPost/NewPost.js
@@ -1,31 +1,53 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import axios from 'axios'
 
-import './NewPost.css';
+import './NewPost.css'
 
 class NewPost extends Component {
-    state = {
-        title: '',
-        content: '',
-        author: 'Max'
+  state = {
+    title: '',
+    content: '',
+    author: 'Thanh'
+  }
+
+  postDataHandler = () => {
+    const post = {
+      title: this.state.title,
+      content: this.state.content,
+      author: this.state.author
     }
 
-    render () {
-        return (
-            <div className="NewPost">
-                <h1>Add a Post</h1>
-                <label>Title</label>
-                <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
-                <label>Content</label>
-                <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
-                <label>Author</label>
-                <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
-                    <option value="Max">Max</option>
-                    <option value="Manu">Manu</option>
-                </select>
-                <button>Add Post</button>
-            </div>
-        );
-    }
+    axios.post('/posts', post).then(response => console.log(response))
+  }
+
+  render() {
+    return (
+      <div className="NewPost">
+        <h1>Add a Post</h1>
+        <label>Title</label>
+        <input
+          type="text"
+          value={this.state.title}
+          onChange={event => this.setState({ title: event.target.value })}
+        />
+        <label>Content</label>
+        <textarea
+          rows="4"
+          value={this.state.content}
+          onChange={event => this.setState({ content: event.target.value })}
+        />
+        <label>Author</label>
+        <select
+          value={this.state.author}
+          onChange={event => this.setState({ author: event.target.value })}
+        >
+          <option value="Thanh">Thanh</option>
+          <option value="Minh">Minh</option>
+        </select>
+        <button onClick={this.postDataHandler}>Add Post</button>
+      </div>
+    )
+  }
 }
 
-export default NewPost;
+export default NewPost
--- ReactJS/app-2/src/containers/Blog/Blog.js
+++ ReactJS/app-2/src/containers/Blog/Blog.js
@@ -1,28 +1,66 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import axios from 'axios'
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
-import './Blog.css';
+import Post from '../../components/Post/Post'
+import FullPost from '../../components/FullPost/FullPost'
+import NewPost from '../../components/NewPost/NewPost'
+import './Blog.css'
 
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
+  state = {
+    posts: [],
+    selectedPostId: null,
+    error: false
+  }
+
+  componentDidMount() {
+    axios
+      .get('/posts')
+      .then(respone => {
+        const posts = respone.data.slice(0, 4)
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: 'Thanh'
+          }
+        })
+        this.setState({ posts: updatedPosts })
+      })
+      .catch(error => {
+        console.log(error)
+        this.setState({ error: true })
+      })
+  }
+
+  postSelectedHandler = id => {
+    this.setState({ selectedPostId: id })
+  }
+
+  render() {
+    let posts = <p style={{ textAlign: 'center' }}>Something wrong...</p>
+    if (!this.state.error) {
+      posts = this.state.posts.map(post => (
+        <Post
+          author={post.author}
+          title={post.title}
+          key={post.id}
+          clicked={() => this.postSelectedHandler(post.id)}
+        />
+      ))
     }
+
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        <section>
+          <FullPost id={this.state.selectedPostId} />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    )
+  }
 }
 
-export default Blog;
+export default Blog
