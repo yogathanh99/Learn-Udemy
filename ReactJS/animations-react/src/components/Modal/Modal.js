adb5d8e852b2103b4ab749fd258640f67e2cf9f9
yogathanh99 <vtthanh@apcs.vn>
2019-08-12 16:15:42 +0700

    [ADD] Learn library react-transition-group for react animation

---
+++ ReactJS/animations-react/src/components/Modal/Modal.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { CSSTransition } from 'react-transition-group';
+
+import './Modal.css';
+
+const animationTiming = {
+  enter: 300,
+  exit: 1000,
+};
+
+const modal = props => {
+  return (
+    <CSSTransition
+      in={props.show}
+      timeout={animationTiming}
+      mountOnEnter
+      unmountOnExit
+      classNames={{
+        enter: '',
+        enterActive: 'ModalOpen',
+        exit: '',
+        exitActive: 'ModalClosed',
+      }}
+    >
+      <div className="Modal">
+        <h1>A Modal</h1>
+        <button className="Button" onClick={props.closed}>
+          Dismiss
+        </button>
+      </div>
+    </CSSTransition>
+  );
+};
+
+export default modal;
