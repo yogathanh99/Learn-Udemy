6d761f31d53666f113204a01827eb9897a939f2a
yogathanh99 <vtthanh@apcs.vn>
2020-05-06 21:54:51 +0700

    [ADD] Data Sanitization for NoQL Injection, XSS and prevent HTTP Parameter Pollution

--- NodeJS/starter/app.js
+++ NodeJS/starter/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const toursRoute = require('./routes/toursRoute');
 const usersRoute = require('./routes/usersRoute');
@@ -27,6 +30,28 @@ app.use('/api', limiter); //apply limiter for route have /api
 
 // Limit json file equal or less than 10kb
 app.use(express.json({ limit: '10kb' }));
+
+// Data Sanitization against NoQL query injection
+app.use(mongoSanitize());
+
+// Data Sanitization against XSS
+app.use(xss());
+
+// Prevent HTTP Parameter Pollution
+// Ex: GET /search?name='Thanh'&name='Vo'
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+
 app.use(express.static(`${__dirname}/public`));
 
 // This is one middleware
