439ede0b75d8cf44d5228791210abbe97f9314ee
yogathanh99 <vtthanh@apcs.vn>
2020-05-06 11:41:49 +0700

    [ADD] More security for backend - against XSS, Brute-Force and Use Cookie

--- NodeJS/starter/app.js
+++ NodeJS/starter/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const toursRoute = require('./routes/toursRoute');
 const usersRoute = require('./routes/usersRoute');
@@ -7,17 +9,27 @@ const globalErrorHandler = require('./controllers/errorsController');
 const AppError = require('./utils/appError');
 
 const app = express();
-//Middleware
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Global Middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-app.use(express.json());
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP. Please try again in an hour!',
+}); // -> In 1 hour, just have max 100 request from same IP
+app.use('/api', limiter); //apply limiter for route have /api
+
+// Limit json file equal or less than 10kb
+app.use(express.json({ limit: '10kb' }));
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, _, next) => {
-  console.log('Hello middleware');
-  next();
-});
+// This is one middleware
 app.use((req, _, next) => {
   req.requestTime = new Date().toISOString();
   next();
--- NodeJS/starter/controllers/authControllers.js
+++ NodeJS/starter/controllers/authControllers.js
@@ -14,6 +14,18 @@ const signToken = (id) => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 3600 * 1000
+    ),
+    httpOnly: true, //Ensure cookie is sent only over HTTP(s) -> against XSS
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true; // Ensure browser only send cookie over HTTPS
+  res.cookie('jwt', token, cookieOptions);
+
+  // Remove password from output
+  user.password = undefined;
 
   res.status(statusCode).json({
     status: 'success',
