736cda01dcc2df5c396be5c41ae723e11536ee48
yogathanh99 <vtthanh@apcs.vn>
2020-05-08 15:44:45 +0700

    [MODIFY] Delete config env

--- NodeJS/starter/server.js
+++ NodeJS/starter/server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
-dotenv.config({ path: './config.local.env' });
+dotenv.config({ path: './.env.local' });
 
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
@@ -15,7 +15,7 @@ mongoose
     useUnifiedTopology: true,
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: false,
   })
   .then(() => console.log('DB connections successful'));
 
